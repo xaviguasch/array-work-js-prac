0a263273c77c5f5982ea47bf4de17d2bee1b1c7c
xaviguasch <xaviguasch@gmail.com>
2021-02-03 12:42:11 +0100

    solved getTotal() exercise

--- src/index.js
+++ src/index.js
@@ -14,8 +14,20 @@
 // Using "some" write a function that checks an array for a "falsey" value.
 // eg: checkForFalsey([11, NaN, [], ["Angels"]]) => true
 
-function checkForFalsey(arr) {
-  return arr.some((el) => !el)
+// function checkForFalsey(arr) {
+//   return arr.some((el) => !el)
+// }
+
+// console.log(checkForFalsey([11, NaN, [], ['Angels']]))
+
+////////////////////////////////////////////////////////////////////
+
+// Using "reduce" write a function that will return the total number
+// of characters in an array of strings.
+// eg: getTotal(["Rabbit", "Football", "Cracking"]) => 22
+
+function getTotal(arr) {
+  return arr.reduce((acc, word) => acc + word.length, 0)
 }
 
-console.log(checkForFalsey([11, NaN, [], ['Angels']]))
+console.log(getTotal(['Rabbit', 'Football', 'Cracking']))
