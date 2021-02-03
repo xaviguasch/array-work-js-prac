8d5818db31fc3abf673313a6ff4a06ffdb849f80
xaviguasch <xaviguasch@gmail.com>
2021-02-03 11:39:11 +0100

    solved checkForFalsey() exercise

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@
 // Using "map" write a function that converts farenheit to celcius.
 // eg: getCelcius([23, 140, 212, 41]) => [-5, 60, 100, 5]
 
-function getCelcius(arr) {
-  return arr.map((temp) => ((temp - 32) * 5) / 9)
+// function getCelcius(arr) {
+//   return arr.map((temp) => ((temp - 32) * 5) / 9)
+// }
+
+// console.log(getCelcius([23, 140, 212, 41]))
+
+////////////////////////////////////////////////////////////////////
+
+// Using "some" write a function that checks an array for a "falsey" value.
+// eg: checkForFalsey([11, NaN, [], ["Angels"]]) => true
+
+function checkForFalsey(arr) {
+  return arr.some((el) => !el)
 }
 
-console.log(getCelcius([23, 140, 212, 41]))
+console.log(checkForFalsey([11, NaN, [], ['Angels']]))
