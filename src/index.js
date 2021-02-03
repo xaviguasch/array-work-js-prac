1edada63668ef010fa3e561bc24cb95055872c64
xaviguasch <xaviguasch@gmail.com>
2021-02-03 10:58:16 +0100

    solved getCelcius() exercise

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@
 // Using "map" write a function that converts farenheit to celcius.
 // eg: getCelcius([23, 140, 212, 41]) => [-5, 60, 100, 5]
 
-function getCelcius() {}
+function getCelcius(arr) {
+  return arr.map((temp) => ((temp - 32) * 5) / 9)
+}
+
+console.log(getCelcius([23, 140, 212, 41]))
